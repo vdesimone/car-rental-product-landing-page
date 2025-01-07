cd95fc791232e552c85d9c06b9950163bb9218c2
vdesimone <173499012+vdesimone@users.noreply.github.com>
2025-01-07 14:59:02 -0500

    Add `LandingPage` structure, `Navbar` functionality, and styling - Add title and fonts to `index.html` - Install dependencies: `react-dom` and `remixicon`

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,8 +1,20 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Fleet from "./Fleet";
+import Reviews from "./Reviews";
+import FAQ from "./FAQ";
 
 function LandingPage() {
 
   return (
     <div className="landing-page-container">
+      <Navbar />
+      <Home />
+      <About />
+      <Fleet />
+      <Reviews />
+      <FAQ />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,78 @@
+import { useState, useEffect } from "react";
 
 function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [selectedSection, setSelectedSection] = useState("home");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+
+      const sections = ["home", "about", "fleet", "reviews", "faq"];
+      let currentSection = "";
+      sections.forEach((sectionId) => {
+        const section = document.getElementById(sectionId);
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= 0 && rect.bottom > 0) {
+          currentSection = sectionId;
+        }
+      });
+      setSelectedSection(currentSection);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToSection = (sectionId) => {
+    document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+  };
 
   return(
-    <>
-    </>
+    <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
+      <div className="company-name">
+        VDrive
+      </div>
+      <ul>
+        <li
+          onClick={() => scrollToSection('home')}
+          className={selectedSection === "home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => scrollToSection('about')}
+          className={selectedSection === "about" ? "active" : ""}
+        >
+          About Us
+        </li>
+        <li
+          onClick={() => scrollToSection('fleet')}
+          className={selectedSection === "fleet" ? "active" : ""}
+        >
+          Fleet
+        </li>
+        <li
+          onClick={() => scrollToSection('reviews')}
+          className={selectedSection === "reviews" ? "active" : ""}
+        >
+          Reviews
+        </li>
+        <li
+          onClick={() => scrollToSection('faq')}
+          className={selectedSection === "faq" ? "active" : ""}
+        >
+          FAQ
+        </li>
+      </ul>
+    </nav>
   );
 }
 
