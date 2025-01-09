1b826a709ada98c089d822644b1fd713c096a531
vdesimone <173499012+vdesimone@users.noreply.github.com>
2025-01-09 16:14:42 -0500

    Add structure and styling for `Home` and `About` pages - Include Poltawski Nowy font in the HTML - Add `blue-gtr.jpg` image to `assets` folder

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,67 @@
+import 'remixicon/fonts/remixicon.css';
 
 function About() {
+  const stats = [
+    {
+      id: 1,
+      num: "200+",
+      desc: "Luxury Vehicles Available",
+    },
+    {
+      id: 2,
+      num: "24/7",
+      desc: "Luxury Vehicle Assistance",
+    },
+    {
+      id: 3,
+      num: "8,000+",
+      desc: "5-Star Reviews",
+    },
+    {
+      id: 4,
+      num: "20+",
+      desc: "Luxury Car Brands Available",
+    },
+  ];
+
+  const reviewStat = stats.find(stat => stat.desc === "5-Star Reviews");
 
   return(
-    <>
-    </>
+    <div id="about" className="full-screen-section">
+      <div className="about-section">
+        <h2>We offer <span>premium services</span> found <br />nowhere else</h2>
+        <div className="about-stats">
+          {stats.map((stat) => {
+            const lastStat = stats[stats.length - 1];
+
+            return [
+              <div className="stat" key={stat.id}>
+                <p className="stat-num">{stat.num}</p>
+                <p className="stat-desc">{stat.desc}</p>
+                {stat === reviewStat && (
+                  <div className="review-stars">
+                    <i className="ri-star-fill"></i>
+                    <i className="ri-star-fill"></i>
+                    <i className="ri-star-fill"></i>
+                    <i className="ri-star-fill"></i>
+                    <i className="ri-star-fill"></i>
+                  </div>
+                )}
+              </div>,
+              stat != lastStat && <div className="vl" key={`vl-${stat.id}`}></div>
+            ];
+          })}
+        </div>
+        <div className="about-desc">
+          <p>
+            At <span>VDrive</span>, we believe driving should be
+            an <span>experience</span>- <br />one that&apos;s synonymous with
+            luxury, performance, and freedom.
+          </p>
+        </div>
+        <button>Learn More</button>
+      </div>
+    </div>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,21 @@
+import blueGTR from "../assets/blue-gtr.jpg";
 
 function Home() {
 
   return (
-    <>
-    </>
+    <div id="home" className="full-screen-section home-section">
+      <div className="home-info">
+        <h1>Rent <span id="elegance">Elegance.</span><br />Drive <span id="power">Power.</span></h1>
+        <div className="home-desc">
+          <p>At VDrive, we offer <span>200+</span> luxury vehicles.</p>
+          <p>Explore our extensive fleet today.</p>
+        </div>
+        <button>Browse Our Fleet</button>
+      </div>
+      <div className="home-img-container">
+        <img className="blue-gtr" src={blueGTR} alt="Blue GTR" />
+      </div>
+    </div>
   );
 }
 
