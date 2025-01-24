60adcca9378fefa0db215493231554cf56fe39d6
vdesimone <173499012+vdesimone@users.noreply.github.com>
2025-01-24 14:23:51 -0500

    Add structure, functionality, and styling for `Fleet` page - Create `carData` to store car info - Organize images for `carData` into `cars` folder

---
+++ src/assets/carData.js
@@ -0,0 +1,35 @@
+import car1 from "../assets/cars/car1.png";
+import car2 from "../assets/cars/car2.png";
+import car3 from "../assets/cars/car3.png";
+import car4 from "../assets/cars/car4.png";
+import car5 from "../assets/cars/car5.jpg";
+import car6 from "../assets/cars/car6.jpg";
+import car7 from "../assets/cars/car7.jpg";
+import car8 from "../assets/cars/car8.png";
+import car9 from "../assets/cars/car9.jpg";
+import car10 from "../assets/cars/car10.jpg";
+import car11 from "../assets/cars/car11.jpg";
+import car12 from "../assets/cars/car12.png";
+import car13 from "../assets/cars/car13.png";
+import car14 from "../assets/cars/car14.png";
+import car15 from "../assets/cars/car15.png";
+
+const carData = [
+  { id: 1, name: "Lamborghini Aventador", year: 2022, image: car1, seats: 2, transmissionType: "Auto", fuelType: "Gas", price: 1600, category: ["New Arrivals"] },
+  { id: 2, name: "Rolls-Royce Wraith", year: 2016, image: car2, seats: 2, transmissionType: "Auto", fuelType: "Gas", price: 2000, category: ["New Arrivals"] },
+  { id: 3, name: "Lamborghini Urus", year: 2023, image: car3, seats: 5, transmissionType: "Auto", fuelType: "Gas", price: 1000, category: ["New Arrivals"] },
+  { id: 4, name: "Nissan GT-R R35", year: 2020, image: car4, seats: 4, transmissionType: "Auto", fuelType: "Gas", price: 700, category: ["New Arrivals"] },
+  { id: 5, name: "Audi R8", year: 2022, image: car5, seats: 2, transmissionType: "Auto", fuelType: "Gas", price: 750, category: ["New Arrivals"] },
+  { id: 6, name: "McLaren 720S", year: 2022, image: car6, seats: 2, transmissionType: "Auto", fuelType: "Gas", price: 1400, category: ["Exotic Vehicles"] },
+  { id: 7, name: "Ferrari LaFerrari", year: 2015, image: car7, seats: 2, transmissionType: "Auto", fuelType: "Gas", price: 3000, category: ["Exotic Vehicles"] },
+  { id: 8, name: "Rolls-Royce Cullinan", year: 2021, image: car8, seats: 5, transmissionType: "Auto", fuelType: "Gas", price: 2000, category: ["Exotic Vehicles"] },
+  { id: 9, name: "Aston Martin DBS", year: 2010, image: car9, seats: 4, transmissionType: "Auto", fuelType: "Gas", price: 1500, category: ["Exotic Vehicles"] },
+  { id: 10, name: "Lamborghini Huracán", year: 2019, image: car10, seats: 2, transmissionType: "Auto", fuelType: "Gas", price: 1350, category: ["Exotic Vehicles"] },
+  { id: 11, name: "Mercedes AMG-GT", year: 2020, image: car11, seats: 2, transmissionType: "Auto", fuelType: "Gas", price: 800, category: ["Luxury Vehicles"] },
+  { id: 12, name: "BMW M4", year: 2018, image: car12, seats: 4, transmissionType: "Auto", fuelType: "Gas", price: 500, category: ["Luxury Vehicles"] },
+  { id: 13, name: "Land Rover Range Rover", year: 2020, image: car13, seats: 5, transmissionType: "Auto", fuelType: "Gas", price: 600, category: ["Luxury Vehicles"] },
+  { id: 14, name: "Porsche 911 Turbo", year: 2022, image: car14, seats: 4, transmissionType: "Auto", fuelType: "Gas", price: 750, category: ["Luxury Vehicles"] },
+  { id: 15, name: "Mercedes G-Wagon", year: 2023, image: car15, seats: 5, transmissionType: "Auto", fuelType: "Gas", price: 700, category: ["Luxury Vehicles"] },
+];
+
+export default carData;
--- src/components/Fleet.jsx
+++ src/components/Fleet.jsx
@@ -1,9 +1,157 @@
+import { useState, useEffect } from "react";
+import carData from "../assets/carData";
 
 function Fleet() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState("New Arrivals");
+  const [carsPerRow, setCarsPerRow] = useState(3);
+
+  const updateCarsPerRow = () => {
+    const width = window.innerWidth;
+    if (width <= 855) {
+      setCarsPerRow(1);
+    } else if (width <= 1200) {
+      setCarsPerRow(2);
+    } else {
+      setCarsPerRow(3);
+    }
+  };
+
+  const filteredCars = carData.filter(
+    (car) =>
+      selectedCategory === "All Vehicles" ||
+      car.category.includes(selectedCategory)
+  );
+
+  const carsToDisplay = filteredCars.slice(currentIndex, currentIndex + carsPerRow);
+
+  const canGoNext = currentIndex + carsPerRow < filteredCars.length;
+
+  const nextSlide = () => {
+    if (canGoNext) {
+      setCurrentIndex(prevIndex => prevIndex + 1);
+    }
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(prevIndex => Math.max(prevIndex - 1, 0));
+  };
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    setCurrentIndex(0);
+  };
+
+  const debounce = (func, delay) => {
+    let timeoutId;
+    return (...args) => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(() => {
+        func.apply(null, args);
+      }, delay);
+    };
+  };
+
+  const debouncedUpdateCarsPerRow = debounce(updateCarsPerRow, 200);
+
+  useEffect(() => {
+    updateCarsPerRow();
+    window.addEventListener("resize", debouncedUpdateCarsPerRow);
+    return () => {
+      window.removeEventListener("resize", debouncedUpdateCarsPerRow);
+    };
+  }, [debouncedUpdateCarsPerRow]);
+
+  useEffect(() => {
+    preloadImages(carsToDisplay);
+  }, [carsToDisplay]);
+
+  const preloadImages = (cars) => {
+    cars.forEach(car => {
+      const img = new Image();
+      img.src = car.image;
+    });
+  };
 
   return (
-    <>
-    </>
+    <div id="fleet" className="full-screen-section">
+      <div className="fleet-section">
+        <h2>Our Fleet</h2>
+        <div className="car-categories">
+          <button onClick={() => {handleCategoryClick("New Arrivals")}}>New Arrivals</button>
+          <button onClick={() => {handleCategoryClick("Exotic Vehicles")}}>Exotic Vehicles</button>
+          <button onClick={() => {handleCategoryClick("Luxury Vehicles")}}>Luxury Vehicles</button>
+          <button onClick={() => {handleCategoryClick("All Vehicles")}}>All Vehicles</button>
+        </div>
+
+        <div className="car-slider">
+          <div className="car-container">
+            {carsToDisplay.map((car) => (
+              <div key={car.id} className="car">
+                <img loading="lazy" src={car.image} alt={car.name} />
+                <div className="car-desc">
+                  <h4>{car.name}</h4>
+                </div>
+                <div className="car-info">
+                  <div className="price">
+                    <span>${car.price}</span>/day
+                  </div>
+                </div>
+                <div className="rent-button">
+                  <button>Rent Now</button>
+                </div>
+                <div className="car-properties">
+                  <div className="car-year property">
+                    <div className="icon">
+                      <i className="ri-roadster-line"></i>
+                    </div>
+                    <p>{car.year}</p>
+                  </div>
+                  <div className="car-seats property">
+                    <div className="icon">
+                      <i className="ri-user-line"></i>
+                    </div>
+                    <p>{car.seats} seater</p>
+                  </div>
+                  <div className="car-transmission-type property">
+                    <div className="icon">
+                      <i className="ri-steering-line"></i>
+                    </div>
+                    <p>{car.transmissionType}</p>
+                  </div>
+                  <div className="car-fuel-type property">
+                    <div className="icon">
+                      <i className="ri-gas-station-line"></i>
+                    </div>
+                    <p>{car.fuelType}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="slider-arrows">
+            <button onClick={prevSlide}>&#8249;</button>
+            <div className="slider-dots">
+              <span
+                className={`dot ${currentIndex === 0 ? "active" : ""}`}
+                onClick={() => setCurrentIndex(0)}
+              />
+              {filteredCars.slice(carsPerRow).map((_, index) => (
+                <span
+                  key={index + 1}
+                  className={`dot ${index + 1 === Math.floor(currentIndex / 1) ? "active" : ""}`}
+                  onClick={() => setCurrentIndex(index + 1)}
+                />
+              ))}
+            </div>
+            <button onClick={nextSlide} disabled={!canGoNext}>&#8250;</button>
+          </div>
+          <button className="view-all-button">View All Vehicles</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
