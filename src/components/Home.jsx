5f47472a8bb1d1c4b3e3cbcaad736a0dd7a34796
vdesimone <173499012+vdesimone@users.noreply.github.com>
2025-02-17 15:47:08 -0500

    Restructure layout and update styles for `Home` page, and update `home-page.jpg` file path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,15 +5,12 @@ function Home() {
   return (
     <div id="home" className="full-screen-section">
       <div className="home-info">
-        <h1>Rent <span id="elegance">Elegance.</span><br />Drive <span id="power">Power.</span></h1>
-        <div className="home-desc">
-          <p>At VDrive, we offer 200+ luxury vehicles.</p>
-          <p>Explore our extensive fleet today.</p>
-          <button>Browse Our Fleet</button>
-        </div>
+        <h1>Premium Car Rental Service</h1>
+        <p>At VDrive, we offer an exclusive selection of over 200 luxury vehicles, designed to provide you with the ultimate driving experience.</p>
+        <button>Browse Our Fleet</button>
       </div>
       <div className="home-img-container">
-        <img className="home-img" src={homeImg} alt="Green BMW Background Image" />
+        <img className="home-img" src={homeImg} alt="Porsche Image" />
       </div>
     </div>
   );
